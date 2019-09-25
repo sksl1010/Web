4e7ad13a70326140e6c240e33d0da7060baf0c3d
sksl1010 <sksl1010@yahoo.co.jp>
2019-09-25 17:32:54 +0900

    201909

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ $(document).ready( function(){
         }
     }
     var tool = new Tool();
-    var c = Shape.Circle( 200, 200);
+    var c = Shape.Circle( 200, 200, 80);
     c.fillColor = 'black';
     var text = new PointText( 200, 200);
     text.justification = 'center' ;
