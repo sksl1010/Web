0fd89b9ad7a4aa0993b1c533fff0c17619fe6cfe
sksl1010 <sksl1010@yahoo.co.jp>
2019-09-25 17:03:32 +0900

    0925_2

---
+++ main.js
@@ -0,0 +1,28 @@
+console.log('main.js loaded')
+$(document).ready( function(){
+    'use strict';
+    paper.install( window );
+    paper.setup( document.getElementById( 'mainCanvas') );
+    var c;
+    for (var x = 25; x< 400; x += 50){
+        for (var y = 25; y < 400; y += 50 ){
+            c = Shape.Circle( x, y, 20);
+            c.fillColor = 'green';
+        }
+    }
+    var tool = new tool();
+    var c = Shape.Circle( 200, 200);
+    c.fillColor = 'black';
+    var text = new PointText( 200, 200);
+    text.justification = 'center' ; text.fillColor = 'white'; text.fontSize = 20;
+    text.center = 'Hello, World!!!';
+
+    tool.onMouseDown = ( event ) {
+        var c = Shape.Circle( event.point, 20);
+        c.fillColor = 'green';
+    };
+   paper.view.drew();
+   console.log( ' jquery - main loaded');
+   
+
+});
